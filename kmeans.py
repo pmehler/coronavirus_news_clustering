86b832f869c489f4429667628a003537e6e6aaf0
pmehler <pmehler@middlebury.edu>
2020-05-11 16:49:32 -0400

    added todo

--- kmeans.py
+++ kmeans.py
@@ -10,6 +10,21 @@ from sklearn.feature_extraction.text import CountVectorizer
 from datetime import datetime
 
 
+
+
+############ - TODO, Ideally by Thursday Morning - ##################
+#Priority List:
+#   write algorithm from scratch
+#   calculate distortion in a cluster
+#       distortion comparison
+#       find optimal k
+#   principal component analysis or stemming
+#   improve labeling, different methods
+#   remove duplicates?
+#   More news articles
+#   visualize
+#       tNe?
+
 stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now", "said"]
 
 #load files from json
@@ -133,6 +148,3 @@ for cIn in range(0,len(clusterText)):
     printKeywords(clusterText[cIn])
     print()
 
-
-#TODO
-#calculate distortion in a cluster

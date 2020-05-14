3fed4cb051f6373f54d050d33b2edd48557850a9
pmehler <pmehler@middlebury.edu>
2020-05-14 19:54:35 -0400

    added kmeans from scratch, tsne plotting, and cluster label via article title of text closest to cluster mean

--- kmeans.py
+++ kmeans.py
@@ -8,22 +8,20 @@ import nltk
 import regex as re
 from sklearn.feature_extraction.text import CountVectorizer
 from datetime import datetime
-
+from kmeans_scratch import find_clusters
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()  # for plot styling
+from scipy.spatial import distance
+from sklearn.metrics import pairwise_distances_argmin
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
 
 
 
 ############ - TODO, Ideally by Thursday Morning - ##################
 #Priority List:
-#   write algorithm from scratch
-#   calculate distortion in a cluster
-#       distortion comparison
-#       find optimal k
-#   principal component analysis or stemming
-#   improve labeling, different methods
-#   remove duplicates?
-#   More news articles
-#   visualize
-#       tNe?
+# merge
+# compare clustering methods
 
 stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now", "said"]
 
@@ -60,17 +58,31 @@ for doc in textbodies:
 
 
 #vectorize
-tv = TfidfVectorizer(min_df=0., max_df=1., use_idf=True)
+tv = TfidfVectorizer(min_df=0., max_df=1., use_idf=True, max_features=8192)
 tv_matrix = tv.fit_transform(cleanText)
 tv_matrix = tv_matrix.toarray()
 mat = np.array(tv_matrix) #holds tfidf vals for each word in each document
 
 
-#run kmeans
-numClusters = 10
-kmeans = KMeans(n_clusters=numClusters)
-kResults = kmeans.fit_predict(mat)#dictinary mapping article text to cluster assignment
+# run kmeans
+numClusters = 12
+centers, kResults, distortion = find_clusters(mat, numClusters)
 
+########################## kmeans elbow #########################
+
+# dis_arr = np.array([])
+# for numClusters in range(1,25):
+#     print(numClusters, "of 25")
+#     centers, kResults, distortion = find_clusters(mat, numClusters)
+#     #print(distortion)
+#     dis_arr = np.append(dis_arr, distortion)
+    
+# plt.figure()
+# plt.plot(dis_arr)
+# plt.title('Elbow: Distortion (WCSS) vs. K')
+# plt.show()
+
+#################################################################
 
 #store files organized by cluster in arrays
 clusters = [] #array of arrays of files
@@ -92,20 +104,27 @@ def datestdtojd (stddate):
     return(jdate)
 
 #print out the titles in each cluster
+titles = []
 average_jdates = []
 for cI in range(0,len(clusters)):
     jdate = 0
+    min_dist=10000
+    index = -1
     print("Cluster ", cI)
     for article in clusters[cI]:
         print(article["title"])
         jdate += datestdtojd(article["date_publish"][0:10])  # we have hh:mm:ss, could be more precise
     average_jdate = jdate/len(clusters[cI]) #what's a better way to represent date of each cluster?
     average_jdates.append(average_jdate)
+    #For each element in mat which is a label in kResults, find the min distance to center of that label
+    for j in range(len(mat[kResults==i])):
+        if min_dist > distance.euclidean(mat[kResults==i][j],centers[i]):
+            min_dist = distance.euclidean(mat[kResults==i][j],centers[i])
+            index = j
+    titles.append(index)
     print()
     print()
     print()
-
-
 
 #print most frequently occurring words in each cluster:
 #currently uses CountVectorizer
@@ -142,9 +161,34 @@ def printKeywords(arrOfText):
 #print out top 5 occuring keywords per cluster
 for cIn in range(0,len(clusterText)):
     print("Cluster "+ str(cIn))
+    print("Title closest to center:",clusters[cIn][titles[cIn]]["title"])
     print(" ", len(clusterText[cIn]), " articles")
     print("Average Julian day of Cluster", average_jdates[cIn])
     print("Keywords:")
     printKeywords(clusterText[cIn])
     print()
+
+
+############################## PLOTTING #####################################
+# pca = PCA(n_components=0.95, random_state=42)
+# X_reduced= pca.fit_transform(mat)
+# kmeans = KMeans(n_clusters=numClusters)
+# y_pred = kmeans.fit_predict(X_reduced)#dictinary mapping article text to cluster assignment
+# tsne = TSNE(verbose=1, perplexity=100, random_state=42)
+# X_embedded = tsne.fit_transform(mat)
+
+# # sns settings
+# sns.set(rc={'figure.figsize':(15,15)})
+
+# # colors
+# palette = sns.hls_palette(12, l=.4, s=.9)
+
+# # plot
+# sns.scatterplot(X_embedded[:,0], X_embedded[:,1], hue=y_pred, legend='full', palette=palette)
+# plt.title('t-SNE with Kmeans Labels')
+# plt.savefig("improved_cluster_tsne.png")
+# plt.show()
+
+
+
 
